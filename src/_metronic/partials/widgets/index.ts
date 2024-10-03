365f437e7ae650af310c161d87cb72ed2a04081b
kavindu-damsith-65 <thilakarathnakdb.21@uom.lk>
2024-10-03 15:49:26 +0530

    12

--- src/_metronic/partials/widgets/index.ts
+++ src/_metronic/partials/widgets/index.ts
@@ -66,7 +66,7 @@ export * from './tables/TablesWidget12'
 export * from './tables/TablesWidget13'
 export * from './tables/TablesWidget14'
 export * from './tables/TablesWidget15'
-
+export * from './tables/TablesWidget16'
 // engage
 export * from './engage/EngageWidget1'
 export * from './engage/EngageWidget2'
